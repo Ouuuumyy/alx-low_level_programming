fdcc672b20be6767aaf5507321f1c399fc704017
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-19 06:29:37 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void){
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
-}
+/**
+ * main - a function that prints a line using printf
+ * return: 0 (success)
+ */
+int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	}
