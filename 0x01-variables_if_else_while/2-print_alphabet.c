892a9adf89ea66d3128ca236113720023b542814
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-19 16:55:54 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 	char alphabet[26]= "abcdefghijklmnopqrstuvwxyz";
-	int i=0;
+	int i = 0;
 
-	for(i=0; i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar( alphabet[i]);
+		putchar(alphabet[i]);
 	}
 	putchar('\n');
 	return (0);
