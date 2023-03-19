f6a1b155f1c0ff2076629720ca7838c5766c0f06
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-19 06:08:29 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
  * our main function prints the following message 'programming is like building a multilingual puzzle
- */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+*/
+int main (void)
+	{
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
-}
+	}
 
