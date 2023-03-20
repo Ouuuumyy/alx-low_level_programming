de082db254e16a4be2a5a33c6f60a32f8465b433
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-20 10:43:51 -0700

    new commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints numbers from 0 to 10
- * using putchar only
- * Return: Always 0
- */
+* main - prints numbers of base 16
+* using putchar only
+* Return: Always 0
+*/
 int main(void)
 {
 	int i;
-
+	
 	for (i = 48 ; i < 58 ; i++)
 	{
 		putchar(i);
+	}
+	for (i = 48 ; i < 55 ; i++)
+	{
+		putchar(49 i);
 	}
 	putchar('\n');
 	return (0);
