2329a6e0f34d1b558cb4af6a2933bc1c90edf199
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-20 09:12:31 -0700

    new commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints numbers from 0 to 10 
+ * main - prints numbers from 0 to 10
  * Return: Always 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0;i <= 10; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		printf("%d\n",i);
+		printf("%d",i);
 	}
+	printf("\n");
 	return (0);
 }
