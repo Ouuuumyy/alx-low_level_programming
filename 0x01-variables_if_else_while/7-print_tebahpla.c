2fd1da416600c12eb290697611520b7b02f5f14f
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-20 09:47:57 -0700

    final update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 	int i;
-	char alp[26]="abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for(i = 26 ; i > 0 ; i--)
+	for (i = 25 ; i >= 0 ; i--)
 	{
-		putchar( alp[i]);
+		putchar(alp[i];
 	}
 	putchar('\n');
 	return (0);
