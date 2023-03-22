ffac699a4202d1df6828c19c7cd294eeb9975ff8
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-22 14:38:53 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of a number
+ * @n: the entered number
+ * Return: the value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
