80cb3882763132288d78d68d2ae641bcd529d1bc
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-22 16:06:25 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,16 @@
  * Return: the number value
  */
 int print_last_digit(int n)
-{       
+{
 	int a;
 
 	if (n < 0)
 		n = -n;
+
 	a = n % 10;
+	if (a < 0)
+		a = -a;
+
 	_putchar(a + '0');
 	
 	return (0);
