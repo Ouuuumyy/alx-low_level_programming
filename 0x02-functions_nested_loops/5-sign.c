23b32a5b638472ddd4297dc43ca160d8c95109fc
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-22 14:21:29 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,17 @@
  * the sign of a number
  * @n: the number to be checked
  * Return: 1 greater than 0
- * Return: 0 is 0
- * Return: -1 is less than 0
+ * 0 if it's equal to 0
+ * -1 is less than 0
  */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
