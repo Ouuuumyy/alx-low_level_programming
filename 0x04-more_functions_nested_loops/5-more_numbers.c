644caf808c3f30216107a6412c8e10322f23d27a
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-03-25 18:13:48 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print numbers fron 0 to 14
+ */
+void more_numbers(void)
+{
+	int i , j;
+
+	for (j = 0; i < 10 ; i++)
+	{
+		for (i = 0; i < 10; i++)
+	{
+	_putchar(i + '0');
+	}
+	for (i = 0; i < 5; i++)
+	{
+		_putchar('1');
+		_putchar(i + '0');
+	}
+	}
+	_putchar('\n');
+}
