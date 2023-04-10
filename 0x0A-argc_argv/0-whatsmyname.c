b21b9397aeee1fda3d8d43afdb703b2a084d3737
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-10 10:34:42 -0700

    task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - function prints the name of the program
- * @argc: the number of arguments 
- * @argv[]: a table contains the arguments
+ * @argc: the number of arguments
+ * @argv: a table contains the arguments
  * Return: 0 Success
  */
 int main(int argc, char *argv[])
 {
 	printf("%s", argv[argc]);
-	
+
 	return (0);
 }
