6816a0be42e1e47b6e31f40b901717d63385dc89
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-10 10:22:51 -0700

    task1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function prints the name of the program
+ * @argc: the number of arguments 
+ * @argv[]: a table contains the arguments
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[argc]);
+	
+	return (0);
+}
