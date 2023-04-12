e881a0d36e84fc3d412e35f82739bd391f5813ec
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-12 13:02:30 -0700

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include < stdlib.h>
+
+/**
+ * create_array: a function that creates an array of chars
+ * @size: size of the table
+ * @c: the first character
+ * Return: s pointer to a char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	str = malloc(sizeof(char) * size);
+	str[0] = c;
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef
+#define
+
+char *create_array(unsigned int size, char c);
+
+#endif
+
