459a8eefa27583fbdb8f931805ff44f25eb86417
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-13 17:39:12 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,8 +12,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	int len1 = 0, len2 = 0;
-	int i = 0, j = 0;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
