4a9795905f66f1f9b89f224ebb83a254b9063f7f
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-19 13:28:02 -0700

    tasks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d = malloc(sizeof(struct dog));
 	
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		exit(1);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
