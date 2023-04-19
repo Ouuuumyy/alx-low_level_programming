1d3376f3c14ed57d0e2709effccc8fa1f940b1a7
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-18 19:36:15 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{
+/**
+ * dog - a structure that defines a new type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
