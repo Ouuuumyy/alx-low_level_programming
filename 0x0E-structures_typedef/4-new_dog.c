4bffb28fb6fefc950132ee676a1e408e124071f7
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-25 07:28:05 -0700

    tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,6 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
+		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
@@ -30,6 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
+		free(dog);
 		free(dog->owner);
 		return (NULL);
 	}
