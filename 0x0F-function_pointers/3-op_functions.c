36b2f00640694e13045f8bdd0a848d47f36dae4a
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-27 04:28:34 -0700

    tasks

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a fucntion that calculates the sum of two num
+ * @a: first num
+ * @b: sec num
+ * Return: the addition
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a fucntion that calculates the sub of two num
+ * @a: first num
+ * @b: sec num
+ * Return: the substraction
+ **/
+int op_sub(int a, int b)
+{
+	return (a-b);
+}
+
+/**
+ * op_mul - a fucntion that calculates the mul of two num
+ * @a: first num
+ * @b: sec num
+ * Return: the multiplication
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a fucntion that calculates the div of two num
+ * @a: first num
+ * @b: sec num
+ * Return: the division
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a fucntion that calculates the mod of two num
+ * @a: first num
+ * @b: sec num
+ * Return: modulo
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
