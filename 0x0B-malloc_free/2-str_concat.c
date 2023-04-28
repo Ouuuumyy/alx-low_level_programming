ea28ae125958be7e78e9a0fac5aa0f3a21881444
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-04-27 19:19:59 -0700

    tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,13 +33,13 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[i] != '\0')
 	{
-		conct[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 
 	while (s2[ii] != '\0')
 	{
-		conct[ii] = s2[ii];
+		concat[ii] = s2[ii];
 		i++;
 	       	ii++;
 	}
