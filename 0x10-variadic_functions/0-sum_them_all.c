179ff72c08528dc2f7c6c52c2e8a260b365088e9
oumaima kadir <kadir.oumaima2001@gmail.com>
2023-05-02 08:42:39 -0700

    tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,9 +17,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(mylist, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(mylist, int);
 
-	va_end(ap);
+	va_end(mylist);
 
 	return (sum);
 }
